c8519a7628aa26e3d9e57f970eb43c90525378b8
QaiserHussain <qaiserh844@gmail.com>
2022-12-28 20:28:00 +0500

    create menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,15 +1,23 @@
-import { colors, AppBar, Toolbar, Typography, IconButton, Avatar, styled, InputAdornment, Input, Popover,useTheme } from '@mui/material'
+import { colors, AppBar, Toolbar, Typography, IconButton, Avatar, styled, InputAdornment, Input, Menu, MenuItem, Box, ListItemIcon, Button, Divider } from '@mui/material'
 import { useState } from 'react'
-import { MdMenu, MdLanguage, MdSearch } from 'react-icons/md'
+import { MdMenu, MdLanguage, MdSearch, MdOutlinePortrait, MdOutlineManageAccounts, MdFavoriteBorder, MdLogout } from 'react-icons/md'
 
 function Navbar() {
     const [showBar, setShowBar] = useState('-100px');
     const [check, setCheck] = useState(true);
-    const [scaling, setScaling] = useState(0);
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
 
     const handleSearchBar = () => { if (showBar === '-100px' && check === false) { setShowBar('0'); } else { setShowBar('-100px'); setCheck(true); } }
-    const handleScaling = () => { if (scaling === 1) { setScaling(0) } else { setScaling(1); } }
-   
+
+    const handleMenu = (e) => {
+        if (!anchorEl) {
+            setAnchorEl(e.currentTarget);
+        } else {
+            setAnchorEl(null);
+        }
+    }
+
     const hidebar = {
         position: 'absolute',
         top: showBar,
@@ -21,21 +29,7 @@ function Navbar() {
         padding: '5px',
         transition: 'ease-in-out 0.5s'
     }
-    const scale = {
-        position: 'absolute',
-        top: '85px',
-        right: '10px',
-        width: '300px',
-        transform: `scale(${scaling})`,
-        transition: 'all ease-in-out 0.2s',
-        height: '300px',
-        padding: '10px',
-        border: '1px solid lightgrey',
-        borderRadius: '10px',
-        // boxShadow: '-1px -1px 5px lightgrey,1px 1px 5px lightgrey',
-        backgroundColor:'white',
-        zIndex:999
-    }
+
     return (
         <>
             <AppBarNav>
@@ -44,11 +38,11 @@ function Navbar() {
                         <Input
                             fullWidth
                             disableUnderline
-                            sx={{ border: '1px solid lightgrey', borderRadius: '50px', padding: '2px 0px 2px 15px', fontSize:'12px' }}
+                            sx={{ border: '1px solid lightgrey', borderRadius: '50px', padding: '2px 0px 2px 15px', fontSize: '12px' }}
                             placeholder='Search location... '
                             endAdornment={
                                 <InputAdornment position='end'>
-                                    <IconButton size='small' sx={{color:colors.orange[600]}} onClick={handleSearchBar}>
+                                    <IconButton size='small' sx={{ color: colors.orange[600] }} onClick={handleSearchBar}>
                                         <MdSearch />
                                     </IconButton>
                                 </InputAdornment>
@@ -61,11 +55,11 @@ function Navbar() {
                             <Input
                                 fullWidth
                                 disableUnderline
-                                sx={{ border: '1px solid lightgrey', borderRadius: '50px', padding: '2px 0px 2px 15px', fontSize:'12px' }}
+                                sx={{ border: '1px solid lightgrey', borderRadius: '50px', padding: '2px 0px 2px 15px', fontSize: '12px' }}
                                 placeholder='Search location... '
                                 endAdornment={
                                     <InputAdornment position='end'>
-                                        <IconButton size='small' sx={{color:colors.orange[600]}} onClick={handleSearchBar}>
+                                        <IconButton size='small' sx={{ color: colors.orange[600] }} onClick={handleSearchBar}>
                                             <MdSearch />
                                         </IconButton>
                                     </InputAdornment>
@@ -86,19 +80,80 @@ function Navbar() {
                             >
                                 <MdSearch />
                             </IconButton>
-                            <IconButton size='large' sx={{ marginRight: '10px' }}><MdLanguage /></IconButton>
+
+                            <IconButton size='large' sx={{ marginRight: '10px' }}>
+                                <MdLanguage />
+                            </IconButton>
+
                             {/* <Credentials component={'div'}>
-                            <IconButton size='large'><MdMenu /></IconButton>
-                            <IconButton><Avatar /></IconButton>
-                        </Credentials> */}
+                                <IconButton size='large'><MdMenu /></IconButton>
+                                <IconButton><Avatar /></IconButton>
+                            </Credentials> */}
 
-                            <IconButton onClick={handleScaling}><Avatar /></IconButton>
-
-                            <Typography component='div' sx={scale}>
-                                menu items
-                            </Typography>
+                            <IconButton
+                                type='button'
+                                id="basic-button"
+                                aria-controls={open ? 'basic-menu' : undefined}
+                                aria-haspopup="true"
+                                aria-expanded={open ? 'true' : undefined}
+                                onClick={handleMenu}
+                            >
+                                <Avatar />
+                            </IconButton>
                         </Icons>
                     </Nav>
+
+
+                    <Menu
+                        id="basic-menu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleMenu}
+                        MenuListProps={{
+                            'aria-labelledby': 'basic-button',
+                        }}
+                    >
+                        <Box sx={{ minWidth:{xs:'250px',sm:'250px',md:'250px'} }}>
+                            <MenuHead>
+                                <Avatar sx={{ height:{ xs:'40px',sm:'40px'}, width:{ xs:'40px',sm:'40px'} }} />
+                                <MenuHeadText>
+                                    <Typography component='div' variant='body1'>Qaiser Hussain</Typography>
+                                    <Typography component='div' variant='caption'>qaiserh844@gmail.com</Typography>
+                                </MenuHeadText>
+                            </MenuHead>
+
+                            <MenuItem onClick={handleMenu}>
+                                <ListItemIcon>
+                                    <MdOutlinePortrait fontSize="large" />
+                                </ListItemIcon>
+                                Profile
+                            </MenuItem>
+                            <MenuItem onClick={handleMenu}>
+                                <ListItemIcon>
+                                    <MdFavoriteBorder fontSize="large" />
+                                </ListItemIcon>
+                                Wishlist
+                            </MenuItem>
+                            <MenuItem onClick={handleMenu}>
+                                <ListItemIcon>
+                                    <MdOutlineManageAccounts fontSize="large" />
+                                </ListItemIcon>
+                                Setting
+                            </MenuItem>
+
+                            <MenuItem onClick={handleMenu}>
+                                <ListItemIcon>
+                                    <MdLogout fontSize="large" />
+                                </ListItemIcon>
+                                Sign out
+                            </MenuItem>
+                            <Divider />
+                            <MenuFoot>
+                                <Button variant='contained' color='warning'> SignIn</Button>
+                                <Button variant='outlined' color='warning' > SignUp</Button>
+                            </MenuFoot>
+                        </Box>
+                    </Menu>
                 </Toolbar>
             </AppBarNav>
         </>
@@ -118,14 +173,13 @@ const Nav = styled(Typography)(({ theme }) => ({
     display: 'flex',
     flex: '1',
     justifyContent: 'space-between',
-    alignItems: 'center',
-    // padding: '10px'
+    alignItems: 'center'
 }))
 
 
 const Logo = styled(Typography)(({ theme }) => ({
     fontSize: '28px',
-    color:colors.orange[600]
+    color: colors.orange[600]
 }))
 
 const SearchBar = styled(Typography)(({ theme }) => ({
@@ -142,13 +196,30 @@ const Icons = styled(Typography)(({ theme }) => ({
     alignItems: 'center',
 }))
 
-const Credentials = styled(Typography)(({ theme }) => ({
-    border: '1px solid lightgrey',
-    borderRadius: '50px',
-    width: '120px',
+const MenuHead = styled(Box)(({ theme }) => ({
     display: 'flex',
-    justifyContent: 'space-between'
+    alignItems: 'center',
+    gap: '20px',
+    padding: '10px',
 }))
+const MenuHeadText = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column'
+}))
+
+const MenuFoot = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    justifyContent: 'center',
+    gap: '20px'
+}))
+// const Credentials = styled(Typography)(({ theme }) => ({
+//     border: '1px solid lightgrey',
+//     borderRadius: '50px',
+//     width: '100px',
+//     display: 'flex',
+//     justifyContent: 'space-between',
+//     padding:'0px'
+// }))
 
 
 
