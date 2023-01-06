97aa7b4b69a2c9761f983daf502218cd2b9006ee
QaiserHussain <qaiserh844@gmail.com>
2023-01-06 22:47:10 +0500

    adjust card

--- pages/reservation/index.jsx
+++ pages/reservation/index.jsx
@@ -5,9 +5,10 @@ import one from '../../assets/one.jpg'
 
 function Reservation() {
     return (
-        <Box>
-            <Box sx={{ flex: 1, display: 'flex', gap: 2, paddingTop: '50px',flexWrap:'wrap' }}>
-                <Box sx={{ flex: 0.8, padding: '10px 40px 10px 10px' }}>
+        <Box sx={{display:'flex',justifyContent:'space-between',flexWrap:'wrap-reverse'}}>
+
+            <Box sx={{ flex: 0.5, paddingTop: '50px', }}>
+                <Box sx={{ flex: 1, padding: '10px 40px 10px 10px' }}>
                     <Box>
                         <Typography variant='h5' component='div'>Hi, Qaiser Hussain </Typography>
                         <Typography variant='caption' component='div'> Review your booking details to continnue</Typography>
@@ -23,20 +24,55 @@ function Reservation() {
                         <Typography variant='h6' component='div'>Guests </Typography>
                         <Typography variant='caption' component='div'> 1 guest </Typography>
                     </Box>
-
                 </Box>
-                <Box sx={{ flex: 0.5 }}>
-                    <Box sx={{ flex: 0.5, border: '1px solid lightgrey', borderRadius: '10px', width: '400px', padding: '20px' }}>
+
+                {/* <Divider sx={{ width: '500px', marginBottom: '10px' }} /> */}
+                   
+                   {/* payment and policies */}
+
+                <Box sx={{ maxWidth: '500px' }}>
+
+                    <Box>PAYMENT METHODS</Box>
+
+                    <Divider sx={{ width: '500px', marginBottom: '10px' }} />
+
+                    <Box sx={{ marginTop: '20px', marginBottom: '20px' }}>
+                        <Typography component='div' variant='h5'> Cancellation policy: Firm 95 days </Typography>
+                        <Typography component='div' variant='caption'>
+                            Cancel up to 95 days before your trip and only pay 50% of the total accommodation fees for the reservation.
+                            Cancel within 95 days of your trip and the reservation is non-refundable.
+                            Cancel before 3:00 PM on Jan 7 and only get a refund of the hospitality fee.
+                        </Typography>
+                    </Box>
+
+                    <Divider sx={{ width: '500px', marginBottom: '10px' }} />
+
+                    <Box sx={{ marginTop: '20px', marginBottom: '20px' }}>
+                        <Typography component='div' variant='h5'> Your reservation won’t be confirmed until the Host accepts your request (within 24 hours).  </Typography>
+                        <Typography component='div' variant='caption'> You won’t be charged until then. </Typography>
+                    </Box>
+
+                    <Divider sx={{ width: '500px', marginBottom: '10px' }} />
+
+                    <Button variant='contained' color='warning'>Request to Book</Button>
+                </Box>
+
+            </Box>
+            
+            {/* reservation card deails */}
+
+            <Box sx={{ flex: 1, padding:{sm:'50px 0 0 0',md:'50px 0 0 50px'} }}>
+                    <Box sx={{ maxWidth:'400px',minWidth:'250px', border: '1px solid lightgrey', borderRadius: '10px', padding: '20px',marginLeft:'auto',marginRight:'auto' }}>
                         <Box sx={{ display: 'flex', gap: 1, marginBottom: '10px' }}>
                             <Box>
                                 <Image src={one} width={80} height={80} />
                             </Box>
-                            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+                            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between',flex:1 }}>
                                 <Box>
                                     <Typography variant='caption' component='div'>Entire home</Typography>
                                     <Typography variant='body1' component='div'>home title or post title</Typography>
                                 </Box>
-                                <Box sx={{ display: 'flex', gap: 2 }}>
+                                <Box sx={{ display: 'flex', justifyContent:'space-between' }}>
                                     <Typography variant='caption' component='div'>5.0 8 reviews</Typography>
                                     <Typography variant='caption' component='div'>Host: Qaiser hussain</Typography>
                                 </Box>
@@ -71,36 +107,6 @@ function Reservation() {
                         </Typography>
                     </Box>
                 </Box>
-            </Box>
-
-            <Divider sx={{ width: '500px', marginBottom: '10px' }} />
-
-            <Box sx={{ maxWidth: '500px' }}>
-
-                <Box>PAYMENT METHODS</Box>
-
-                <Divider sx={{ width: '500px', marginBottom: '10px' }} />
-
-                <Box sx={{ marginTop: '20px', marginBottom: '20px' }}>
-                    <Typography component='div' variant='h5'> Cancellation policy: Firm 95 days </Typography>
-                    <Typography component='div' variant='caption'>
-                        Cancel up to 95 days before your trip and only pay 50% of the total accommodation fees for the reservation.
-                        Cancel within 95 days of your trip and the reservation is non-refundable.
-                        Cancel before 3:00 PM on Jan 7 and only get a refund of the hospitality fee.
-                    </Typography>
-                </Box>
-
-                <Divider sx={{ width: '500px', marginBottom: '10px' }} />
-
-                <Box sx={{ marginTop: '20px', marginBottom: '20px' }}>
-                    <Typography component='div' variant='h5'> Your reservation won’t be confirmed until the Host accepts your request (within 24 hours).  </Typography>
-                    <Typography component='div' variant='caption'> You won’t be charged until then. </Typography>
-                </Box>
-
-                <Divider sx={{ width: '500px', marginBottom: '10px' }} />
-
-                <Button variant='contained' color='warning'>Request to Book</Button>
-            </Box>
 
         </Box>
     )
